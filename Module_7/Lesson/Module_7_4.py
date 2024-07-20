a921bc8c743720d0f29fe149cdeb3edf9e8508ba
Tweenex <tweenex@mail.ru>
2024-07-20 11:36:32 +0300

    Lesson_7_4

---
+++ Module_7/Lesson/Module_7_4.py
@@ -0,0 +1,30 @@
+team1_num = 5
+print("В команде Мастера кода участников: %d !" % team1_num)
+
+team1_num = 5
+team2_num = 6
+print("Итого сегодня в командах участников: %d и %d !" % (team1_num, team2_num))
+
+#Использование format()
+
+score_2 = 42
+print("Команда Волшебники данных решила задач: {} !".format(score_2))
+
+team2_time = 18015.2
+print("Волшебники данных решили задачи за {:.1f} с !".format(team2_time))
+
+#Использование f-строк
+
+score_1 = 40
+score_2 = 42
+print(f"Команды решили {score_1} и {score_2} задач.")
+
+#исход соревнования
+
+challenge_result = "Победа команды Волшебники данных!"
+print(f"Результат битвы: {challenge_result}")
+
+tasks_total = 82
+time_avg = 350.4
+print(f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg:.1f} секунды на задачу!")
+
