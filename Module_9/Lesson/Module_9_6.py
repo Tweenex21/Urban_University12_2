c3361c5b3db74d7f8d1284d07ee9315b8e9d363b
Tweenex <tweenex@mail.ru>
2024-08-06 20:25:32 +0300

    Lesson_9_6

---
+++ Module_9/Lesson/Module_9_6.py
@@ -0,0 +1,10 @@
+def all_variants(text):
+    length = len(text) + 1
+    for i in range(1, length):
+        for j in range(length - i):
+            yield text[j: j + i]
+
+
+a = all_variants("abc")
+for i in a:
+    print(i)
