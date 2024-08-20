7ac7ce1c545f47ab51c1ae30b0178345215069ef
Tweenex <tweenex21@gmail.com>
2024-08-20 20:28:31 +0300

    Lesson_10_3

---
+++ Module_11/Lesson1/11_1.py
@@ -0,0 +1,28 @@
+import requests
+import pandas as pd
+import numpy as np
+#Все примеры по работе с библиотеками закоментил дабы не было каши.
+#Пример_1
+# r = requests.get('https://api.github.com/events')
+# print("Заголовки ответа:")
+# for header, value in r.headers.items():
+#     print(f"{header}: {value}")
+#
+# # Вывод содержимого ответа
+# print("\nСодержимое ответа:")
+# print(r.text)
+
+#Пример_2
+# text = pd.read_csv('Pandas.txt')
+# print(text)
+# print(text.info())
+
+#Пример_3
+# array_1d = np.array([1, 2, 3, 4, 5])
+# print(array_1d)
+# #Сложение
+# array_sum = array_1d + 3
+# print(array_sum)
+# # Сумма элементов
+# sum_value = np.sum(array_1d)
+# print(sum_value)
